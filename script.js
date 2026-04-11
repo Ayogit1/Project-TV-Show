961a8d1886d778ca21beea1923ad2d58375834bf
Ayodeji Ayorinde <godleadco@hotmail.com>
2026-04-11 14:46:05 +0100

    Level0

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 //You can edit ALL of the code here
+
+const filmCard = document.createElement("section");
+
+const title = document.createElement("h1");
+const director = document.createElement("p");
+director.textContent = film.director;
+title.textContent = film.title;
+filmCard.appendChild(title);
+filmCard.appendChild(director);
+
+document.body.appendChild(filmCard);
+
+
+////////////////////
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
